15481231044f0d2f0388a942ce49bfcb030a55fb
MB <M@PL-CLT-044.pagero.local>
2020-11-18 11:19:52 +0100

    Add project files.

---
+++ listträning/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace listträning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //ctr+k, ctrl+d = snygga till
+            //ett sätt
+
+            int[] arrayOfNumbers = new int[5];
+            arrayOfNumbers[0] = 13;
+            arrayOfNumbers[1] = 22;
+            arrayOfNumbers[2] = 3;
+            arrayOfNumbers[3] = 93;
+            arrayOfNumbers[4] = 1;
+
+            Console.WriteLine("Check if you pick a lucky number");
+            string nr = Console.ReadLine(); 
+            int myNr = Convert.ToInt32(nr);
+
+            for (int i = 0; i < arrayOfNumbers.Length; i++)
+            {
+                if (myNr==arrayOfNumbers[i])
+                {
+                    Console.WriteLine("winner winner chicken dinner");
+
+                }
+                else
+                {
+                    Console.WriteLine("no win for you");
+                }
+            }
+
+        }
+    }
+}
